cc47de6b8f3e0e0e3bb70febd505fba3fb303364
gigih2117 <beatrizgiovanna378@gmail.com>
2024-12-03 11:32:33 -0300

    Add files via upload

---
+++ aula 2/aula02.py
@@ -0,0 +1,12 @@
+print("ola bom diaa")
+print("um otimo dia a todos")
+
+nome_usuario = 'gigih2117' # tipo -> str = string
+idade_usuario = 17 # tipo -> int = integer
+altura_usuario = 1.69 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+print('Seja Bem vindo', nome_usuario)
+print ('seja bem-vinda', nome_usuario)
+print ('sua idade é', idade_usuario, 'anos')
+print ('você tem', altura_usuario, "metro.")
+print ('você é maior de idade?', maior_idade)
